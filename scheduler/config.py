5c43d6713577a033b7e46c5fcdc58fd6c36cdbee
Benjamin Akhras <b@akhras.at>
2024-10-05 18:28:24 +0200

    improve subpath configuration

--- scheduler/config.py
+++ scheduler/config.py
@@ -1,4 +1,5 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import field_validator, model_validator, ValidationInfo
 from datetime import datetime
 
 
@@ -18,9 +19,25 @@ class Settings(BaseSettings):
     GIT_INFO: dict[str, str] | None = None
     CACHE_TYPE: str = "SimpleCache"
     CACHE_DEFAULT_TIMEOUT: int = 300
-    TARANIS_CORE_URL: str = "http://taranis/api"
+    TARANIS_CORE_URL: str = ""
+    TARANIS_CORE_HOST: str = "core:8080"
+    TARANIS_BASE_PATH: str = "/"
     SSL_VERIFICATION: bool = False
     REQUESTS_TIMEOUT: int = 60
+
+    @field_validator("TARANIS_BASE_PATH", mode="before")
+    def ensure_start_and_end_slash(cls, v: str, info: ValidationInfo) -> str:
+        if not v or v == "/":
+            return "/"
+
+        return f"/{v.strip('/')}/"
+
+    @model_validator(mode="after")
+    def set_taranis_core(self):
+        if self.TARANIS_CORE_URL:
+            return self
+        self.TARANIS_CORE_URL = f"http://{self.TARANIS_CORE_HOST}{self.TARANIS_BASE_PATH}api"
+        return self
 
 
 Config = Settings()
